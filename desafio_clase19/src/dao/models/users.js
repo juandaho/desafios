9b71ff13defd85c0208e29bc7a1f048bdde4cefc
Juan Holguin <juandaho@gmail.com>
2023-09-03 18:25:10 -0500

    desafió clase 19

---
+++ desafio_clase19/src/dao/models/users.js
@@ -0,0 +1,30 @@
+
+import mongoose from "mongoose";
+
+const userCollection = 'users';
+
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    age: Number,
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+});
+
+const userModel = mongoose.model(userCollection, userSchema);
+
+export default userModel;
